f29ba99eeb50b591503bf90acc18816b610dbfd1
Arun Vydianathan <avydianathan@reputation.com>
2021-05-13 16:51:17 +0530

    Added port number arg

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-mvn exec:java -Dexec.mainClass="com.manning.lp.cloud.iot.endtoend.CloudiotPubsubExampleMqttDevice" -Dexec.args="-project_id=$1 -registry_id=CloudIOT_Sync -device_id=Serene_Sensor -private_key_file=rsa_private_pkcs8 -algorithm=RS256"
+mvn exec:java -Dexec.mainClass="com.manning.lp.cloud.iot.endtoend.CloudiotPubsubExampleMqttDevice" -Dexec.args="-project_id=$1 -mqtt_bridge_port=$2 -registry_id=CloudIOT_Sync -device_id=Serene_Sensor -private_key_file=rsa_private_pkcs8 -algorithm=RS256"
